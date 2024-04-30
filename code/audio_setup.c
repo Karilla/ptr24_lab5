8e63f3cbe336a084f4be9707febbe71437305b8b
Karilla <benoit.delay@heig-vd.ch>
2024-04-30 16:51:12 +0200

    init

---
+++ code/audio_setup.c
@@ -0,0 +1,32 @@
+#include "audio_setup.h"
+
+void acquisition_task(void *cookie)
+{
+    Priv_audio_args_t *priv = (Priv_audio_args_t *)cookie;
+
+    int err;
+    // Calculate the minimum frequency of the task
+    uint32_t audio_task_freq = (SAMPLING * sizeof(data_t)) / (FIFO_SIZE);
+    // Calculate the period of the task with a margin to ensure not any data is lost
+    uint64_t period_in_ns = S_IN_NS / (audio_task_freq + PERIOD_MARGIN);
+
+    if ((err = rt_task_set_periodic(NULL, TM_NOW, period_in_ns)) < 0)
+    {
+        rt_printf("Could not set period of acquisition task\n");
+        return;
+    }
+
+    while (priv->ctl->running)
+    {
+        // Get the data from the buffer
+        ssize_t read = read_samples(priv->samples_buf, FIFO_SIZE * NB_CHAN);
+        if (read)
+        {
+            // Write the data to the audio output
+            write_samples(priv->samples_buf, read);
+        }
+
+        rt_task_wait_period(NULL);
+    }
+    rt_printf("Terminating acquisition task\n");
+}
---
+++ code/audio_setup.h
@@ -0,0 +1,52 @@
+/*****************************************************************************
+ * Author: A.Gabriel Catel Torres
+ *
+ * Version: 1.0
+ *
+ * Date: 30/04/2024
+ *
+ * File: audio_setup.h
+ *
+ * Description: functions and defines used to launch Xenomai tasks to perform
+ * the acquisition of the audio data coming from the microphone input and write
+ * it to the audio output.
+ *
+ ******************************************************************************/
+
+#ifndef AUDIO_SETUP_H
+#define AUDIO_SETUP_H
+
+#include <alchemy/task.h>
+#include <stdbool.h>
+
+#include "audio_utils.h"
+#include "control.h"
+
+#define AUDIO_ACK_TASK_PRIORITY 50
+
+#define AUDIO_FILENAME "output.wav"
+
+// Audio defines
+#define SAMPLING 48000.
+#define FIFO_SIZE 256
+#define NB_CHAN 2
+#define PERIOD_MARGIN 25
+
+#define S_IN_NS 1000000000UL
+
+typedef struct Priv_audio_args
+{
+    Ctl_data_t *ctl;
+    RT_TASK acquisition_rt_task;
+    data_t *samples_buf;
+} Priv_audio_args_t;
+
+/**
+ * \brief Read the data from the audio input and writes it to the
+ * audio output.
+ *
+ * \param cookie pointer to private data. Can be anything
+ */
+void acquisition_task(void *cookie);
+
+#endif // AUDIO_SETUP_H
