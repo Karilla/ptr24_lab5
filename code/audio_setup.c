64767008797e167a7906672ae650871003eaece8
Karilla <benoit.delay@heig-vd.ch>
2024-05-14 17:51:53 +0200

    Added message

--- code/audio_setup.c
+++ code/audio_setup.c
@@ -67,16 +67,34 @@ void acquisition_task(void *cookie)
     // Calculate the period of the task with a margin to ensure not any data is lost
     uint64_t period_in_ns = S_IN_NS / (audio_task_freq + PERIOD_MARGIN);
 
-    uint16_t fft_buffer[8192];
+    uint16_t fft_buffer[FFT_BUFFER_SIZE];
 
     uint32_t nb_byte_readen = 0;
 
-    if (rt_task_create(&treatment, "Treatement Task", 0, 99, T_JOINABLE))
+    RT_QUEUE *mailbox_treatment;
+    RT_QUEUE *mailbox_logging;
+    rt_queue_create(mailbox_treatment, "Audio Message Queue", sizeof, Q_UNLIMITED, Q_FIFO);
+    rt_queue_create(mailbox_logging, "Audio Message Queue", 1000, Q_UNLIMITED, Q_FIFO);
+
+    Priv_audio_sub_args_t treatment_task_args;
+    treatment_task_args.ctl = priv->ctl;
+    treatment_task_args.mail_box = mailbox_treatment;
+
+    if (rt_task_spawn(treatment_task_args.sub_task, "Treatment Task", 0, 50, T_JOINABLE, treatment_task, &treatment_task_args))
     {
-        rt_printf("Error during creation of treatement task\nExiting.....\n");
-        exit(EXIT_SUCCESS);
+        rt_printf("Error while launching treatment task\n");
+        exit(EXIT_FAILURE);
     }
 
+    Priv_audio_sub_args_t monitoring_task_args;
+    monitoring_task_args.ctl = priv->ctl;
+    monitoring_task_args.mail_box = mailbox_logging;
+
+    if (rt_task_spawn(monitoring_task_args.sub_task, "Monitoring Task", 0, 50, T_JOINABLE, monitoring_task, &monitoring_task_args))
+    {
+        rt_printf("Error while launching monitoring task\n");
+        exit(EXIT_FAILURE);
+    }
     if ((err = rt_task_set_periodic(NULL, TM_NOW, period_in_ns)) < 0)
     {
         rt_printf("Could not set period of acquisition task\n");
@@ -98,7 +116,10 @@ void acquisition_task(void *cookie)
             {
                 // On demarre la tache traitement et on lui envoie le buffer plus la taille du buffer
                 nb_byte_readen = 0;
-                rt_task_start(&treatement, &treatment_task, priv);
+                message_treatement_t message;
+                message.samples_buf = priv->samples_buf;
+                rt_queue_send(mailbox_treatment, &message, sizeof(message_treatement_t), Q_NORMAL);
+                memset(priv->samples_buf, 0, FFT_BUFFER_SIZE)
             }
             else
             {
